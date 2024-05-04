b5488e1cef6a8a99593ece122758fca0a0c9ed63
Sri Raman Prasad <91125096+sriramanprasad@users.noreply.github.com>
2024-05-04 13:35:35 +0530

    Create contact.py

---
+++ pages/contact.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+# Streamlit app
+st.title('ABOUT PAGE')
+
+st.write("""
+    Knowing the top trending videos and hashtags on YouTube is crucial for content creators
+    for several reasons:
+        **:blue[colored]**
+    1. **:red[Content Relevance]**: Trending videos and hashtags provide insights into what
+    viewers are interested in right now. Creating content around trending topics
+    increases the chances of reaching a larger audience.
+
+    2. **:red[SEO Benefits]**: Using trending hashtags in your video descriptions and tags
+    can improve the visibility of your videos in search results, leading to more
+    views and engagement.
+
+    3. **:red[Inspiration and Ideas]**: Trending videos can inspire content creators with
+    new ideas and approaches for their own videos, helping them stay relevant
+    and innovative.
+
+    4. **:red[Competitive Analysis]**: Monitoring trends can also help creators understand
+    what their competitors are doing and how they can differentiate their content
+    to stand out.
+
+    By leveraging trendlist data, content creators can optimize their content strategy
+    and increase their chances of success on YouTube.
+""")
